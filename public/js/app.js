4408a8d87f1e35023876bb2748748b12e8077f6e
Jasson Qasqant <jcascante@gmail.com>
2010-10-15 00:55:02 -0600

    Simple formatting

--- public/js/app.js
+++ public/js/app.js
@@ -1,54 +1,52 @@
-
-
-(function($){
-  var zebralize = function(){
-      $("tr:nth-child(odd)").addClass("odd");
-      $("tr:nth-child(even)").addClass("even");
-  }
-  var externalize = function(){
-      $("body").delegate("a", "click", function(e){
-        var a = new RegExp('/' + window.location.host + '/');
-           if(!a.test(this.href)) {
-               e.preventDefault();
-               e.stopPropagation();
-               window.open(this.href, '_blank');
-           }
-     })
-  }
-
-
-
-return init = {
-    app: function(){ 
-        externalize();
-        
-        if ($("article.agenda")[0]){
-          zebralize();  
-        }
-        
+(function($) {
+    
+    var zebralize = function() {
+        $("tr:nth-child(odd)").addClass("odd");
+        $("tr:nth-child(even)").addClass("even");
     }
-} 
+    
+    /* loads all non-local links on _blank */
+    var externalize = function() {
+        $("body").delegate("a", "click",
+        function(e) {
+            var a = new RegExp('/' + window.location.host + '/');
+            if (!a.test(this.href)) {
+                e.preventDefault();
+                e.stopPropagation();
+                window.open(this.href, '_blank');
+            }
+        })
+    }
 
+    return init = {
+        app: function() {
+            externalize();
 
+            if ($("article.agenda")[0]) {
+                zebralize();
+            }
+
+        }
+    }
 
 })(window.jQuery);
 
-$(function() { init.app() });  
+$(function() { init.app() });
 
-window.log = function(){
-  log.history = log.history || []; 
-  log.history.push(arguments);
-  if(this.console){
-    console.log( Array.prototype.slice.call(arguments) );
-  }
+
+
+window.log = function() {
+    log.history = log.history || [];
+    log.history.push(arguments);
+    if (this.console) {
+        console.log(Array.prototype.slice.call(arguments));
+    }
 };
 
-(function(doc){
-  var write = doc.write;
-  doc.write = function(q){ 
-    log('document.write(): ',arguments); 
-    if (/docwriteregexwhitelist/.test(q)) write.apply(doc,arguments);  
-  };
+(function(doc) {
+    var write = doc.write;
+    doc.write = function(q) {
+        log('document.write(): ', arguments);
+        if (/docwriteregexwhitelist/.test(q)) write.apply(doc, arguments);
+    };
 })(document);
-
-
